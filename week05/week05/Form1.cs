b6175b015d600b8f924a3b300f4fea4d84cdd613
katger98@gmail.com <katger98@gmail.com>
2020-10-09 18:35:25 +0200

    Save

--- week05/week05/Form1.cs
+++ week05/week05/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -62,6 +63,23 @@ namespace week05
                 value += (decimal)last.Price * item.Volume;
             }
             return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.Default))
+            {
+                foreach (var port in Portfolio)
+                {
+                    sw.Write(port.Index);
+                    sw.Write(";");
+                    sw.WriteLine(port.Volume);
+                }
+            }
+
+
         }
     }
 }
