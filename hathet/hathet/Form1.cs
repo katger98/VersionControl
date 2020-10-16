702c10c5f705584ea39c2578bd4d8ddc99b8b999
katger98@gmail.com <katger98@gmail.com>
2020-10-16 19:11:06 +0200

    7. feladat

--- hathet/hathet/Form1.cs
+++ hathet/hathet/Form1.cs
@@ -23,6 +23,7 @@ namespace hathet
             WebSzolg();
             dataGridView1.DataSource = Rates;
             DrawChart();
+            RefreshData();
         }
         private void WebSzolg()
         {
@@ -30,9 +31,9 @@ namespace hathet
 
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
             };
 
             var response = mnbService.GetExchangeRates(request);
@@ -57,7 +58,7 @@ namespace hathet
             }
         }
 
-       private void DrawChart()
+        private void DrawChart()
         {
             chartRateData.DataSource = Rates;
 
@@ -74,7 +75,26 @@ namespace hathet
             chartArea.AxisX.MajorGrid.Enabled = false;
             chartArea.AxisY.MajorGrid.Enabled = false;
             chartArea.AxisY.IsStartedFromZero = false;
-        }    
-    
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
     }
 }
