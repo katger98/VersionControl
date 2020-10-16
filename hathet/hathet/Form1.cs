117e2fcbba969e29443cd03f3fcc64fa65ea00d9
katger98@gmail.com <katger98@gmail.com>
2020-10-16 18:42:24 +0200

    2, 3. feladat

---
+++ hathet/hathet/Form1.cs
@@ -0,0 +1,37 @@
+﻿using hathet.MnbServiceReference;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hathet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            WebSzolg();
+        }
+        private void WebSzolg()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames ="EUR",
+                startDate="2020-01-01",
+                endDate="2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+        }
+    }
+}
