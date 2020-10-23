f06bb0543e2be19596f74f2d01b853f489cec92f
katger98@gmail.com <katger98@gmail.com>
2020-10-23 18:38:39 +0200

    Classok

---
+++ gyak7/gyak7/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak7.Entities
+{
+   public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public byte Age { get; set; }
+        public double Probability { get; set; }
+    }
+}
