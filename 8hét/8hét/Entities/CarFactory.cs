ad293dd0d46e0a4531a0830b49307c96cf2a5af7
katger98@gmail.com <katger98@gmail.com>
2020-11-11 12:40:00 +0100

    i dont even knwo

--- 8hét/8hét/Entities/CarFactory.cs
+++ 8hét/8hét/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace _8hét.Entities
 {
-    public class BallFactory: IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
