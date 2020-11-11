3eba71f19cd99da673f7ac79812444e07f71c3c4
katger98@gmail.com <katger98@gmail.com>
2020-11-11 12:34:46 +0100

    Car

---
+++ 8hét/8hét/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using _8hét.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8hét.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image smolcar = Image.FromFile("Images/car.png");
+            g.DrawImage(smolcar, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
